4057586c5907861c192f99311ee344ed2bcd82d9
sochetanie <allagorik2901@gmail.com>
2017-06-14 13:17:42 -0400

    done

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,34 @@
 function getFahrenheits(result){
   // Your code goes here
+  return result.hourly_forecast.map(x => {return x.temp.english})
 }
 
 function getHours(result){
   // Your code goes here
+  return result.hourly_forecast.map(x => {return x.FCTTIME.hour})
 }
 
 function generateDataSet(labels, data) {
   // Your code goes here
+  var data = {
+    labels: labels,
+    datasets: [
+      {
+        label: "Hourly Weather for New York",
+        fillColor: "rgba(220,220,220,0.2)",
+        strokeColor: "rgba(220,220,220,1)",
+        pointColor: "rgba(220,220,220,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)",
+        data: data
+      }
+    ]
+  }
+  return data
 }
 
 function makeAjaxRequest(endpoint, success) {
   // Your code goes here
+  $.ajax({ url: 'endpoint', dataType : 'jsonp', success: success })
 }
